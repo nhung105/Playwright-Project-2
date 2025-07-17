7d1e4a55b91c63efb4c87741d9fcc7d9b1a0f67d
NhungBeo <nguyennhung105124@gmail.com>
2025-07-17 22:30:19 +0700

    Refactor test structure and improve formatting in alert, date-picker, and drop-down tests

--- tests/alert.test.ts
+++ tests/alert.test.ts
@@ -2,20 +2,27 @@ import { expect, test } from "@playwright/test";
 
 const alertButton = "alert-btn";
 const confirmButton = "confirm-btn";
-const alertText = "Hello! I am an alert box!!"
+const alertText = "Hello! I am an alert box!!";
 
 test.beforeEach(async ({ page }) => {
-    await page.goto('https://qa-practice.netlify.app/');
-    await page.getByText('Alerts').click();
-})
-test('Click Alert', async ({ page }) => {
+  await page.goto("https://qa-practice.netlify.app/");
+  await page.getByText("Alerts").click();
+});
+test.afterEach(async ({ page }) => {
+  await page.close();
+});
 
-    page.once('dialog', async (dialog) => {
-        expect(dialog.message()).toBe(alertText)
-        await dialog.accept();
-
-    })
-    await page.locator('#alert-btn').click();
-})
-
-
+test("Click Alert", async ({ page }) => {
+  page.on("dialog", async (dialog) => {
+    expect(dialog.message()).toBe(alertText);
+    await dialog.accept();
+  });
+  await page.locator("#alert-btn").click();
+});
+test("Click button confirm", async ({ page }) => {
+  page.on("dialog", async (dialog) => {
+    expect(dialog.message()).toBe("Press a button!\nEither OK or Cancel.");
+    await dialog.dismiss();
+  });
+  await page.locator("#confirm-btn").click();
+});
