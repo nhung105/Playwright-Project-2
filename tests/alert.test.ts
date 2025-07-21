c785ba0336ce77da40ffc152c5ed8b69d9032238
nhung105 <nguyennhung105124@gmail.com>
2025-07-21 17:29:33 +0700

    Add files via upload

--- tests/alert.test.ts
+++ tests/alert.test.ts
@@ -2,27 +2,20 @@ import { expect, test } from "@playwright/test";
 
 const alertButton = "alert-btn";
 const confirmButton = "confirm-btn";
-const alertText = "Hello! I am an alert box!!";
+const alertText = "Hello! I am an alert box!!"
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("https://qa-practice.netlify.app/");
-  await page.getByText("Alerts").click();
-});
-test.afterEach(async ({ page }) => {
-  await page.close();
-});
+    await page.goto('https://qa-practice.netlify.app/');
+    await page.getByText('Alerts').click();
+})
+test('Click Alert', async ({ page }) => {
 
-test("Click Alert", async ({ page }) => {
-  page.on("dialog", async (dialog) => {
-    expect(dialog.message()).toBe(alertText);
-    await dialog.accept();
-  });
-  await page.locator("#alert-btn").click();
-});
-test("Click button confirm", async ({ page }) => {
-  page.on("dialog", async (dialog) => {
-    expect(dialog.message()).toBe("Press a button!\nEither OK or Cancel.");
-    await dialog.dismiss();
-  });
-  await page.locator("#confirm-btn").click();
-});
+    page.once('dialog', async (dialog) => {
+        expect(dialog.message()).toBe(alertText)
+        await dialog.accept();
+
+    })
+    await page.locator('#alert-btn').click();
+})
+
+
