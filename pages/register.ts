9e7fb79e2f8915f1d90ea651cb6f32169b368d4f
nhung105 <nguyennhung105124@gmail.com>
2025-07-23 17:57:27 +0700

    Add files via upload

--- pages/register.ts
+++ pages/register.ts
@@ -1,17 +1,33 @@
 import { Locator, Page } from "@playwright/test";
 
+
+type RegisterFormFields = {
+
+}
 class RegisterForm {
     readonly page: Page;
     readonly firstName: Locator;
     readonly lastName: Locator;
     readonly phone: Locator;
+    readonly country: Locator;
+    readonly email?: Locator;
+    readonly password?: Locator;
+    readonly agreeToTerm: Locator;
+    readonly registerButton: Locator;
+
     constructor(page: Page) {
         this.page = page;
         this.firstName = page.locator('#firstName')
         this.lastName = page.locator('#lastName')
         this.phone = page.locator('#phone')
+        this.country = page.locator('#countries_dropdown_menu')
+        this.email = page.locator('#emailAddress')
+        this.password = page.locator('#password');
+        this.agreeToTerm = page.locator('#exampleCheck1')
+        this.registerButton = page.locator('#registerBtn')
     }
     async navigate() {
         await this.page.goto('https://qa-practice.netlify.app/register');
     }
+
 }
