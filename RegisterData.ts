0b0b03d0ddb5303e30006c97a97c23d318a86207
nhung105 <nguyennhung105124@gmail.com>
2025-07-24 17:57:29 +0700

    Add files via upload

---
+++ RegisterData.ts
@@ -0,0 +1,10 @@
+import { RegisterFormFields } from "../pages/register"
+import RegisterForm from "../pages/register"
+export const data: RegisterFormFields = {
+    firstName: "abc",
+    lastName: "nguyen",
+    phone: 123456,
+    country: "Andorra",
+    email: "bnn@gmail.com",
+    password: "nnnn"
+}
