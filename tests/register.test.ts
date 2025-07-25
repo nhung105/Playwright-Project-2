eefff805e6fdd1f7fff80dad359ef902f7018083
nhung105 <nguyennhung105124@gmail.com>
2025-07-25 17:46:34 +0700

    Add files via upload

--- tests/register.test.ts
+++ tests/register.test.ts
@@ -1,11 +1,31 @@
 import test, { expect } from "@playwright/test";
-import RegisterPage from "../../Playwright Project 2/QA-Practice-Playwright-Automation-master/pages/RegisterPage";
-let registerPage: RegisterPage;
+import { allFieldsData, requiredData } from "../data/RegisterData";
+import RegisterForm from "../pages/register";
+let registerPage: RegisterForm;
 
 test.beforeEach(async ({ page }) => {
-    registerPage = new RegisterPage(page)
+    registerPage = new RegisterForm(page)
+    await registerPage.navigate()
 })
 
-test('', async ({ page }) => {
-    expect
+test('fill all fields', async ({ page }) => {
+    await registerPage.fillForm(allFieldsData)
 })
+
+test('fill only required fields', async ({ page }) => {
+    await registerPage.fillForm(requiredData)
+})
+
+test('dcdcd', async ({ page }) => {
+    await registerPage.email.fill("abc@gmail.com")
+    await registerPage.registerButton.click();
+    const message = await registerPage.password.evaluate((el) => {
+        return (el as HTMLInputElement).validationMessage
+    })
+    expect(message).toBe('Please fill out this field.')
+})
+
+
+
+
+
